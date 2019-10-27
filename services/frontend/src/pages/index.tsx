9a314d4fa939cee52856fd943e3658e621c85236
Takayuki Ochiai <go.ahead.oti@gmail.com>
2019-10-27 16:24:06 +0900

    ルートマスキングの実装

--- services/frontend/src/pages/index.tsx
+++ services/frontend/src/pages/index.tsx
@@ -4,12 +4,13 @@ import Link from "next/link";
 import Layout from "../components/mylayout";
 
 type PostProps = {
+  id: string;
   title: string;
 };
 
-const PostLink = ({ title }: PostProps) => (
+const PostLink = ({ id, title }: PostProps) => (
   <li>
-    <Link href={`/post?title=${title}`}>
+    <Link as={`/p/${id}`} href={`/post?title=${title}`}>
       <a>{title}</a>
     </Link>
   </li>
@@ -19,9 +20,9 @@ const Index = () => (
   <Layout>
     <h1>My blog</h1>
     <ul>
-      <PostLink title="Hello Next.js" />
-      <PostLink title="Learn Next.js is awesome" />
-      <PostLink title="Deploy apps with Zeit" />
+      <PostLink id="hello-nextjs" title="Hello Next.js" />
+      <PostLink id="learn-nextjs" title="Learn Next.js is awesome" />
+      <PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
     </ul>
   </Layout>
 );
