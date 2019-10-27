b14a0571f8b8e90e5e9b7a6339dee7697642c9c5
Takayuki Ochiai <go.ahead.oti@gmail.com>
2019-10-27 19:14:07 +0900

    変数名を微修正

--- services/frontend/src/server/index.ts
+++ services/frontend/src/server/index.ts
@@ -5,26 +5,26 @@ import * as ENV from "./constant";
 const dev = process.env.NODE_ENV !== "production";
 
 (async () => {
-  const app = Express();
+  const server = Express();
   const nextApp = next({ dev, dir: "./src/client" });
   const handle = nextApp.getRequestHandler();
   await nextApp.prepare();
 
-  app.get("/about", (req, res) => {
+  server.get("/about", (req, res) => {
     return nextApp.render(req, res, "/about", req.query);
   });
 
-  app.get("/p/:id", (req, res) => {
+  server.get("/p/:id", (req, res) => {
     const actualPage = "/post";
     const queryParams = { title: req.params.id };
     return nextApp.render(req, res, actualPage, queryParams);
   });
 
-  app.use((req, res) => {
+  server.use((req, res) => {
     handle(req, res);
   });
 
-  app.listen(ENV.APP_PORT, ENV.APP_HOST, (err: Express.Errback) => {
+  server.listen(ENV.APP_PORT, ENV.APP_HOST, (err: Express.Errback) => {
     if (err) throw err;
     console.log(`> Ready on http://${ENV.APP_HOST}:${ENV.APP_PORT}`);
   });
