ac80d635c169b7da535d5a02d2dc000b4815e563
Takayuki Ochiai <go.ahead.oti@gmail.com>
2020-05-04 17:32:50 +0900

    ユーザーと組織の中間テーブルを作成

---
+++ services/console/db/migrate/20200430105935_create_organization_users.rb
@@ -0,0 +1,12 @@
+class CreateOrganizationUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :organizations_users do |t|
+      t.references  :user,         foreign_key: true
+      t.references  :organization, foreign_key: true
+      t.timestamps
+    end
+
+    add_index :organizations_users, [:user_id, :organization_id], unique: true
+    add_index :organizations_users, [:organization_id, :user_id], unique: true
+  end
+end
